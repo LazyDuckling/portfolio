a6943c6478a8f959a7103daf6a6de7fa16061b5a
LazyDuckling <emi@lazyduckling.dev>
2022-09-14 13:21:23 -0500

    Desktop layout done

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,19 @@
 "use strict";
 
+// Get content classes for nav, content and footer
+let navElement = document.getElementsByClassName("nav__content");
+let contentElement = document.getElementsByClassName("content");
+let footerElement = document.getElementsByClassName("footer__content");
+
+// Get the nav width
+let navWidth = navElement[0].clientWidth;
+
+// Function to convert pixel value of nav width to rem, then assign it to elements
+function pixelsToRem(pixels) {
+    let contentWidth = pixels / 16 + "rem";
+    contentElement[0].style.width = (contentWidth);
+    footerElement[0].style.width = (contentWidth);
+}
+
+// Run pixelsToRem function on page load
+window.addEventListener = ("Load", pixelsToRem(navWidth));
