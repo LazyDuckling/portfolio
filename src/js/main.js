6c62f4132a00a5107f0354c4a3e532372160985b
LazyDuckling <emi@lazyduckling.dev>
2022-11-13 11:52:32 -0600

    Work page added

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// -------------------------------------------------------------------------------------------
+
 // Get content classes for nav, content and footer
 let navElement = document.getElementsByClassName("nav__content");
 let contentElement = document.getElementsByClassName("content");
@@ -16,3 +18,14 @@ function pixelsToRem(pixels) {
 
 // Run pixelsToRem function on page load
 window.addEventListener = ("Load", pixelsToRem(navWidth));
+
+// -------------------------------------------------------------------------------------------
+
+// Get the current year to automatically update the copyright
+let currentYear = new Date().getFullYear();
+
+// Get the footer copyright element
+let copyrightElement = document.getElementsByClassName("footer__copyright");
+
+// Set the text in the element
+copyrightElement[0].innerHTML = "© Copyright " + currentYear; 
