8a98d748496b88f8dd1fb6ab15fe39e2a08ada2d
LazyDuckling <emi@lazyduckling.dev>
2022-11-14 11:06:18 -0600

    Mobile layout finished

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,29 @@ let currentYear = new Date().getFullYear();
 let copyrightElement = document.getElementsByClassName("footer__copyright");
 
 copyrightElement[0].innerHTML = "© Copyright " + currentYear; 
+
+
+// Toggle the hamburger menu on/off
+let hamburgerState = false;
+
+let hamburgerElement = document.getElementsByClassName("nav__content-icon");
+let menuElement = document.getElementsByClassName("nav__content-mobile");
+
+function hamburgerInit() {
+    hamburgerElement[0].addEventListener("click", hamburgerToggle);
+}
+
+function hamburgerToggle() {
+    if (hamburgerState == false) {
+        menuElement[0].style = "display: grid";
+        hamburgerState = true;
+
+    } else {
+        menuElement[0].style = "display: none";
+        hamburgerState = false;
+    }
+
+
+}
+
+hamburgerInit();
