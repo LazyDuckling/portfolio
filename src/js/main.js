708ddb5ddaa5377b64a10d5b56a31b9fdd97cef5
LazyDuckling <emi@lazyduckling.dev>
2022-11-14 07:38:02 -0600

    Removed javascript for footer width and redid it with CSS instead

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,5 @@
 "use strict";
 
-// -------------------------------------------------------------------------------------------
-
-// Get content classes for nav, content and footer
-let navElement = document.getElementsByClassName("nav__content");
-let contentElement = document.getElementsByClassName("content");
-let footerElement = document.getElementsByClassName("footer__content");
-
-// Get the nav width
-let navWidth = navElement[0].clientWidth;
-
-// Function to convert pixel value of nav width to rem, then assign it to elements
-function pixelsToRem(pixels) {
-    let contentWidth = pixels / 16 + "rem";
-    footerElement[0].style.width = (contentWidth);
-}
-
-// Run pixelsToRem function on page load
-window.addEventListener = ("Load", pixelsToRem(navWidth));
-
-// -------------------------------------------------------------------------------------------
-
 // Get the current year to automatically update the copyright
 let currentYear = new Date().getFullYear();
 
